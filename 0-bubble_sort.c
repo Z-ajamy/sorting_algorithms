8be41418fb1e72824e50dce24ada138639efabcc
Z-ajamy <abdorahman0283@gmail.com>
2025-04-10 18:31:03 +0200

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,8 +19,8 @@ void bubble_sort(int *array, size_t size)
                 flag = 1;
             }
             
-            print_array(array, size);
         }
+        print_array(array, size);
         if (!flag)
             break;
     }
