18e9d960996da74fc1447cac12ac8910460b579a
Z-ajamy <abdorahman0283@gmail.com>
2025-04-10 18:28:20 +0200

    ?

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@ void bubble_sort(int *array, size_t size)
     
     for (i = 0; i < size; i++)
     {
+        flag = 0;
         for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
